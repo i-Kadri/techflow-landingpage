2352349652c69c230259bb70f34ef47935da3192
Kadri <samhenriks17@gmail.com>
2025-10-24 16:54:13 +0100

    adjusted hero-container (height)

--- src/components/herosection.js
+++ src/components/herosection.js
@@ -3,26 +3,31 @@
 function HeroSection() {
   return (
     <div className="Hero-container">
-      <div className="Hero-div">
-        <h1>Build Amazing Digital Products</h1>
-        <p>
-          where We help businesses create stunning web applications and digital
-          experiences that drive growth and engagement.
-        </p>
-        <div className="buttons">
-          <button className="Hero-button">Get Started</button>
-          {/* <button className="Hero-button Hero-button-secondary">
-            Learn More
-          </button> */}
+      <div className="Hero-box">
+        <div className="Hero-div">
+          <h1>
+            Build Amazing{" "}
+            <span style={{ color: "#3b3eee" }}>Digital Products</span>
+          </h1>
+          <p>
+            Where we help businesses create stunning web applications and
+            digital experiences that drive growth and engagement.
+          </p>
+          <div className="buttons">
+            <button className="hero-btn">Get Started</button>
+            {/* <button className="Hero-button Hero-button-secondary">
+              Learn More
+            </button> */}
+            <button className="learn-btn">Learn More</button>
+          </div>
         </div>
-      </div>
-
-      <div className="Hero-image">
-        <img
-          src={require("../assets/codebase.jpg")}
-          alt="Hero"
-          className="hero-img"
-        />
+        <div className="Hero-image">
+          <img
+            src={require("../assets/codebase.jpg")}
+            alt="Hero"
+            className="hero-img"
+          />
+        </div>
       </div>
     </div>
   );
