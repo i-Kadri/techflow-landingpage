8b593fe729a359f0733f73bfca12fe19aac98539
Kadri <samhenriks17@gmail.com>
2025-10-24 18:12:14 +0100

    adjusted hero-text

--- src/components/herosection.js
+++ src/components/herosection.js
@@ -3,32 +3,33 @@
 function HeroSection() {
   return (
     <div className="Hero-container">
-      <div className="Hero-box">
-        <div className="Hero-div">
-          <h1>
-            Build Amazing{" "}
-            <span style={{ color: "#3b3eee" }}>Digital Products</span>
-          </h1>
-          <p>
-            Where we help businesses create stunning web applications and
-            digital experiences that drive growth and engagement.
-          </p>
-          <div className="buttons">
-            <button className="hero-btn">Get Started</button>
-            {/* <button className="Hero-button Hero-button-secondary">
+      {/* <div className="Hero-box"> */}
+      <div className="Hero-div">
+        <h1>
+          Build Amazing <br />
+          <span style={{ color: "#3b3eee" }}>Digital Products</span>
+        </h1>
+        <p>
+          Where we help businesses create stunning web applications and digital
+          experiences that drive growth and engagement.
+        </p>
+
+        <div className="buttons">
+          <button className="hero-btn">Get Started</button>
+          {/* <button className="Hero-button Hero-button-secondary">
               Learn More
             </button> */}
-            <button className="learn-btn">Learn More</button>
-          </div>
-        </div>
-        <div className="Hero-image">
-          <img
-            src={require("../assets/codebase.jpg")}
-            alt="Hero"
-            className="hero-img"
-          />
+          <button className="learn-btn">Learn More</button>
         </div>
       </div>
+      <div className="Hero-image">
+        <img
+          src={require("../assets/codebase.jpg")}
+          alt="Hero"
+          className="hero-img"
+        />
+      </div>
+      {/* </div> */}
     </div>
   );
 }
