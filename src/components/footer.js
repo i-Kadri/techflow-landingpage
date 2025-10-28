30ee4cf633efb524b8f936c3e276bc161f6b4305
Kadri <samhenriks17@gmail.com>
2025-10-28 23:34:11 +0100

    little spacing adjustments within the <footer> sectn

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,32 @@
-import { FaBolt } from "react-icons/fa";
+import { FaBolt, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
     <footer className="footer-box">
-      <div className="logo-icon">
+      <div className="footer-icon">
         <FaBolt style={{}} />
         <h3>TechFlow</h3>
       </div>
-      <div className="logo-text">TechFlow</div>
+      {/* <div className="logo-text">TechFlow</div> */}
       <p>© {new Date().getFullYear()} TechFlow. All rights reserved.</p>
+
+      <div className="footer-links">
+        <span>
+          {" "}
+          <FaFacebook />{" "}
+        </span>
+        <span>
+          {" "}
+          <FaTwitter />{" "}
+        </span>
+        <span>
+          <FaInstagram />{" "}
+        </span>
+        <span>
+          <FaLinkedin />
+        </span>
+      </div>
     </footer>
   );
 }
