5daee64447a0623f5a06a65567725cf147eef046
Kadri <samhenriks17@gmail.com>
2025-10-28 23:12:41 +0100

    footer adjustment

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,13 @@
+import { FaBolt } from "react-icons/fa";
+
 function Footer() {
   return (
     <footer className="footer-box">
+      <div className="logo-icon">
+        <FaBolt style={{}} />
+        <h3>TechFlow</h3>
+      </div>
+      <div className="logo-text">TechFlow</div>
       <p>© {new Date().getFullYear()} TechFlow. All rights reserved.</p>
     </footer>
   );
